b6ed1ee02f718c6a652d61ae9876b471713f06f5
Artur Piasecki <apiasecki93@gmail.com>
2023-06-26 14:44:50 +0200

    finished QR card

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html, js}"],
+  theme: {
+    extend: {
+      colors: {
+        customBackground: "#d0dcea",
+        customWhite: "#ffffff",
+        cH1: '#1f3251',
+        cP: '#7b879d',
+
+      },
+    },
+  },
+  plugins: [],
+};
